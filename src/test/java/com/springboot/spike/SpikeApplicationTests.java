9942a97b2a72522eb1dcb143ce26c9888b9e4373
1026635038@qq.com <Waitforyou321>
2022-03-22 23:40:01 +0800

    秒杀项目创建-20220322

---
+++ src/test/java/com/springboot/spike/SpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.spike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
